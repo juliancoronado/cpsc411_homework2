36e2d98e5c4bf30aff67f5081f115cb17ed2033e
Julian Coronado <julian.c28@gmail.com>
2020-11-19 00:14:45 -0800

    Added top comment for details

--- app/src/main/java/com/example/cpsc411_hw2/Claim.kt
+++ app/src/main/java/com/example/cpsc411_hw2/Claim.kt
@@ -1,3 +1,8 @@
+// CPSC 411-01 - Fall 2020 (Android)
+// Julian Coronado
+// CWID: 889838066
+// Homework #2 - Due November 26th, 2020
+
 package com.example.cpsc411_hw2
 
 // only needs title and date
